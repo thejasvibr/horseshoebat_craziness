2423a9d3ce6ebf765e541daaf1fe3c0873c47b12
thejasvibr <tbeleyur@orn.mpg.de>
2019-07-10 17:54:57 +0200

    add script to upsample the test dataset

---
+++ audio_video_sync/upsampling_LED_signal_04072019.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""Upsample the LED signal from 04072019 to a uniform 25 fps 
+
+Created on Wed Jul 10 17:53:30 2019
+
+@author: tbeleyur
+"""
+
+import pandas as pd
+import os 
+import glob 
+
+
+
+

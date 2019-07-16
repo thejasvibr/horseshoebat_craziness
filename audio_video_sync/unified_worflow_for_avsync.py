debe12e7a1497827caac99ce4cc9481cf6fb2230
tbeleyur <tbeleyur@orn.mpg.de>
2019-07-16 11:24:59 +0200

    initial commit

---
+++ audio_video_sync/unified_worflow_for_avsync.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Writing a unified work flow for the AV sync finally
+Created on Tue Jul 16 11:23:34 2019
+
+@author: tbeleyur
+"""
+
+import numpy as np 
+import scipy.signal as signal 

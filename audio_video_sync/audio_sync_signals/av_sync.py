969a03b83ebcaa64ddca02eac58d268ab5dc79f5
tbeleyur <tbeleyur@orn.mpg.de>
2019-07-23 17:19:12 +0200

    change peak threshold to 0.5 because a lot of peaks seem to be getting missed

--- audio_video_sync/audio_sync_signals/av_sync.py
+++ audio_video_sync/audio_sync_signals/av_sync.py
@@ -6,6 +6,7 @@ Created on Wed Jun 26 13:50:37 2019
 @author: tbeleyur
 """
 import glob
+import warnings
 import multiprocessing
 import time
 import matplotlib.pyplot as plt
@@ -15,26 +16,25 @@ import peakutils as peak
 import scipy.signal as signal 
 import soundfile as sf
 
-
-
-def get_audio_sync_signal(sync_channel, min_durn=0.08,fs=250000, **kwargs):
+def get_audio_sync_signal(sync_channel, **kwargs):
     '''Creates a square type wave that takes on 3 values: -1,0,1.
     The pulse durations (ON/OFF) is calculated by the time gap between 
     adjacent peaks. If the pulse duration is < min_durn, then this pulse
     is set to 0. If the pulse duration is >= min_durn then it is set to 
     either +1/-1 depending on which type of pulse it is. 
 
+    TODO:
+        1) If consecutive +ve/+ve pulses or -ve/-ve pulses are encountered
+        throw a warning and proceed by filling it with an OFF state ? 
+       
+        2) accept min threshold and min distance for peak detection through kwargs
+          instead of the current get_peaks_parallelised
+
     Parameters
     ----------
     
     sync_channel : array-like. 1x Nsamples
 
-    min_durn : float>0.
-               Any pulses that are below min_durn will be set to a 
-               bunch of zeros of equivalent duration. Defaults to 0.08 sec.
-
-    fs : int >0
-        Frequency of sampling in Hz. Defaults to 250 kHz.
 
     Keyword Arguments
     -----------------
@@ -42,6 +42,9 @@ def get_audio_sync_signal(sync_channel, min_durn=0.08,fs=250000, **kwargs):
     parallel : Boolean. 
                If True then the peak finding is done with pool.map 
                , else it is done serially with plain map syntax. 
+    
+    min_distance : see get_peaks
+    min_level : see get_peaks
 
     Returns
     --------
@@ -66,22 +69,21 @@ def get_audio_sync_signal(sync_channel, min_durn=0.08,fs=250000, **kwargs):
     pos_sync[sync<0] = 0
     neg_sync = sync.copy()
     neg_sync[sync>0] = 0
+
     # get indices of positive and negative peaks 
 
-#    pos_peaks = peak.indexes(pos_sync, 0.5, min_distance)
-#    neg_peaks = peak.indexes(abs(neg_sync), 0.5, min_distance)
-
-    
     print('begin peak  processing')
     
-    
-    if kwargs['parallel']:
+    doit_parallely = kwargs.get('parallel', False)  
+    map_inputs = [(pos_sync,kwargs), (neg_sync,kwargs)]
+
+    if doit_parallely:
         pool = multiprocessing.Pool(2)
-        pos_peaks, neg_peaks = pool.map(get_peaks_parallelised, 
-                                            [pos_sync, neg_sync])
+        pos_peaks, neg_peaks = pool.map(get_peaks, 
+                                            map_inputs)
     else:
-        pos_peaks, neg_peaks = map(get_peaks_parallelised, 
-                                            [pos_sync, neg_sync])
+        pos_peaks, neg_peaks = map(get_peaks, 
+                                            map_inputs)
         
     print('peak finding processing done')
 
@@ -114,33 +116,11 @@ def get_audio_sync_signal(sync_channel, min_durn=0.08,fs=250000, **kwargs):
     else:
         last_segment = np.zeros(nsamples_lastsegment)
     many_segments.append(last_segment)
-    
-        
-        
-    
-    
-#    
-#    durations_proper = np.concatenate(( np.array([0]),sorted_peaks,np.array([sync.size])))
-#    pulse_durations = np.diff( abs(durations_proper)) /float(fs)
-#    cumulative_durations = np.cumsum(pulse_durations)
-#    reassembled_signal = np.array([])    
-#
-#    all_pulses = []
-#    for pulse_durn, peak_type, cuml_durn in zip(pulse_durations,
-#                                                np.append(sorted_peaks,sorted_peaks[-1]),
-#                                                          cumulative_durations):
-#        if pulse_durn>=min_durn:
-#            pulse = np.ones(int(pulse_durn*fs))*multiplyby(peak_type)
-#        elif pulse_durn<=min_durn:
-#            pulse = np.zeros(int(pulse_durn*fs))
-#            
-#        all_pulses.append(pulse)
-#    # re-assmeble the signal after suppressing the very short pulses:
     reassembled_signal = np.concatenate(many_segments)
     
     return(reassembled_signal)
 
-def get_pulse_state(index1, index2):
+def get_pulse_state(index1, index2, **kwargs):
     '''index1 and index2 are two integers
     
     Parameters
@@ -158,11 +138,18 @@ def get_pulse_state(index1, index2):
     length = np.diff(np.abs([index1,index2]))
     
     peak_types = tuple([each_peak >0 for each_peak in [index1, index2]])
-    pulse_state = pulse_state_dictionary[peak_types]
-    return(pulse_state, length)
+    pulse_state = pulse_state_dictionary.get(peak_types)
+    if pulse_state is None:
+        warnings.warn('The consecutive peaks of same type occured at samples:'+str(index1)+' ,'+str(index2))
+        warnings.warn(sametype_spike1 +'\n '+sametype_spike2)
+        return(False, length)        
+    else:
+        return(pulse_state, length)
 
 
 pulse_state_dictionary = {(True, False): True, (False,True): False}
+sametype_spike1 = 'There are two consecutive pulses that are rising or dropping!'
+sametype_spike2 = 'The signal between this sametype spike region has been treated as an OFF'
 
 def make_onoff_segment(pulse_state_ison, length):
     '''
@@ -173,15 +160,38 @@ def make_onoff_segment(pulse_state_ison, length):
         return(np.zeros(length))
 
 
-def video_sync_signal(vid_sync, fs=22.0):
-    '''
-    '''
+def get_peaks(X):
+    ''' Gets the peaks by taking the |X| and doing peak finding.
+    Parameters
+    -----------
+        X  : tuple with 2 entries
+             entry 1 is the input signal, which is a 1D array-like object
+             entry 2 is the keyword argument dictionary 
 
+    Keyword Arguments
+    ------------------
+    min_distance : integer
+                   Number of  samples between two consecutive peaks
+                   Defauls to #samples equivalent to 140ms gap @
+                   250 kHz (0.14 * 250000 samples)
+                   See peakutils.peak.indexes
 
-def figure_out_if_ONorOFF(first_peak, second_peak):
+    min_level : 1>= float >0
+                Minimum level to be reached for a sample to be considered a peak. 
+                See peakutils.peak.indexes. 
+                Defaults to 0.7
+
+    Returns
+    -------
+    det_peaks : array-like
+                Samples at which peaks were detected. 
     '''
-    '''
-    
+    input_signal, kwargs = X
+    min_distance = kwargs.get('min_distance',int(250000*0.07*2))
+    min_level = kwargs.get('min_level', 0.5)
+    det_peaks = peak.indexes(np.abs(input_signal), 
+                             min_level, min_distance)
+    return(det_peaks)
 
 
 def multiplyby(peaktype):
@@ -190,9 +200,6 @@ def multiplyby(peaktype):
     else:
         return(0)
 
-def get_peaks_parallelised(X, min_distance=int(250000*0.07*2)):
-        det_peaks = peak.indexes(np.abs(X), 0.7, min_distance)
-        return(det_peaks)
 
 def make_time_axis(X,fs=250000):
     t = np.linspace(0, X.size/float(fs), X.size)
@@ -201,19 +208,26 @@ def make_time_axis(X,fs=250000):
 
 
 if __name__ == '__main__':
-    durns = np.arange(0.2, 2.0, 0.08)
-    ons=[];offs=[];
-    for  i in range(50):
-        ons.append(np.random.choice(durns,1))
-        offs.append(np.random.choice(durns,1))
+    #    durns = np.arange(0.2, 2.0, 0.08)
+    #    ons=[];offs=[];
+    #    for  i in range(50):
+    #        ons.append(np.random.choice(durns,1))
+    #        offs.append(np.random.choice(durns,1))
+    #    
+    #    actual_LED_onoff = []
+    #    fs = 250000
+    #    for on,off in zip(ons,offs):
+    #        actual_LED_onoff.append(np.ones(int(on*fs)))
+    #        actual_LED_onoff.append(np.zeros(int(off*fs)))
+    #    
+    #    full_onoff = np.concatenate(actual_LED_onoff)
+    #    just_peaks = np.diff(full_onoff)
+    #    reconstr = get_audio_sync_signal(just_peaks, parallel=True)
+    audiofs = 250000
+    audio, fs = sf.read('T0000096.WAV', start=int(35*audiofs))
+    sync = audio[:,3]
+    reconstr_audio = get_audio_sync_signal(sync, parallel=True)
+    #np.save('1sec_Reconstr_audio',reconstr_audio[:250000])
     
-    actual_LED_onoff = []
-    fs = 250000
-    for on,off in zip(ons,offs):
-        actual_LED_onoff.append(np.ones(int(on*fs)))
-        actual_LED_onoff.append(np.zeros(int(off*fs)))
     
-    full_onoff = np.concatenate(actual_LED_onoff)
-    just_peaks = np.diff(full_onoff)
-    reconstr = get_audio_sync_signal(just_peaks, parallel=False)
   
